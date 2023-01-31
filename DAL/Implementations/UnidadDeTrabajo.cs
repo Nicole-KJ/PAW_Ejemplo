d231f1de9783ea683c1d6dfc33971d97b066153b
Keren Nicole Jimenez Fernandez <72894324+Nicole-KJ@users.noreply.github.com>
2023-01-31 17:26:57 -0600

    update

---
+++ DAL/Implementations/UnidadDeTrabajo.cs
@@ -0,0 +1,49 @@
+﻿
+using DAL.Implementations;
+using DAL.Interfaces;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class UnidadDeTrabajo<T> : IDisposable where T : class
+    {
+        private readonly NorthWindContext context;
+        //public IDALGenerico<Queja> quejaDAL;
+        //public IDALGenerico<TablaGeneral> tablaDAL;
+        public IDALGenerico<T> genericDAL;
+
+
+        public UnidadDeTrabajo(NorthWindContext _context)
+        {
+            context = _context;
+            genericDAL = new DALGenericoImpl<T>(context);
+
+        }
+
+        public bool Complete()
+        {
+            try
+            {
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                string msj = e.Message;
+                return false;
+            }
+
+        }
+
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
